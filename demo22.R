279e34926b8c9f4e924cb8278c9aac933b3dc0eb
unknown <prz@bu.edu>
2022-11-20 22:26:12 -0500

    runze comit

---
+++ demo22.R
@@ -0,0 +1,136 @@
+library(knitr)
+library(kableExtra)
+library(tidyverse)
+library(rnaturalearth)
+library(sf)
+library(rgdal)
+library(maps)
+library(mapdata)
+library(tmap)
+library(tmaptools)
+library(tidycensus)
+library(sf)
+library(rmapshaper)
+
+library(ggplot2)
+
+
+getwd()
+setwd("D:/Rresources")
+
+
+
+## Create a precise map of Boston
+
+library("jsonlite")
+
+boston<-fromJSON("City_of_Boston_Boundary.geojson") #This data comes from https://data.boston.gov/dataset/city-of-boston-boundary
+
+cord<-boston$features$geometry$coordinates  # Generate original data of coordinates of Boston boundary
+
+long<-c()
+lat<-c()
+group<-c()
+
+for (i in 1:114) {
+  for (j in 1:nrow(cord[[1]][[1]][[i]])) {
+    long<-c(long,cord[[1]][[1]][[i]][j,1])
+    lat<-c(lat,cord[[1]][[1]][[i]][j,2])
+    group<-c(group,i)
+  }
+  
+}
+
+
+boston_cord_p1<-data.frame(long=long,lat=lat,group=group)  #Get clean coordinates data for the first part of Boston
+
+part2<-as.numeric((cord[[1]][[2]]))
+
+boston_cord_p2<-data.frame(long=part2[1:(length(part2)/2)],lat=part2[(length(part2)/2+1):length(part2)],group=rep(115,length(part2)/2)) # Second part of Boston
+
+boston_cord_cp<-rbind(boston_cord_p1,boston_cord_p2) # Combine these two parts
+
+map1<- ggplot(data = boston_cord_cp) +            
+  geom_polygon(aes(x = long, y = lat, group = group), fill = "#FFFFFF" , color = "#33cc33") + coord_fixed(1.3)
+
+map1 # A precise map of Boston
+
+
+## Then we add points on the map, where crimes happened
+
+
+crime_repo<-read.csv("tmpttlexpi6.csv") # This is CRIME INCIDENT REPORTS in Boston in 2022. Download from: https://data.boston.gov/dataset/6220d948-eae2-4e4b-8723-2dc8e67722a3/resource/313e56df-6d77-49d2-9c49-ee411f10cf58/download/tmpdg9fc0p7.csv
+
+
+crime_repo<-crime_repo[(crime_repo$Lat > 40)&(crime_repo$Lat < 42.45)&(crime_repo$Long < -70)&(crime_repo$Long > -71.2),] # We omit missing values in Lat and Long and also ignore the crimes that are far away from Boston(only one case for some unknown reason).
+
+
+crime_repo$DISTRICT<-replace(crime_repo$DISTRICT, crime_repo$DISTRICT == "", "Unknown") # We checked the "DISTRICT" column and found some missing values, we replace these by "Unknown“
+
+
+crime_position <- data.frame(         
+  long = crime_repo$Long, lat = crime_repo$Lat,
+  names = crime_repo$INCIDENT_NUMBER, 
+  district = crime_repo$DISTRICT,
+  stringsAsFactors = FALSE)
+
+map2 <- map1 + geom_point(data=crime_position, aes(x=long, y=lat), color = "#000099", size=0.5)
+
+map2 #Boston map with crimes points on it
+
+#We can look into which district these crimes belong to and mark the positions on the map separately
+
+levels(as.factor(crime_position$district))
+
+# We can see there are total 14 districts in this data
+
+# For example, the district "A1":
+
+map2_A1 <- map1 + geom_point(data=crime_position[crime_position$district == "A1",], aes(x=long, y=lat), color = "#000099", size=0.5)
+
+map2_A1
+
+# The district "Unknown":
+
+map2_Unknown <- map1 + geom_point(data=crime_position[crime_position$district == "Unknown",], aes(x=long, y=lat), color = "#000099", size=0.5)
+
+map2_Unknown
+
+# The district “External”
+
+map2_External <- map1 + geom_point(data=crime_position[crime_position$district == "External",], aes(x=long, y=lat), color = "#000099", size=0.5)
+
+map2_External
+
+
+
+## Next let's show a table that contains the info on which weekdays the crimes happened
+
+
+doweek<-as.factor(crime_repo$DAY_OF_WEEK)
+
+doweek<-factor(doweek,levels = levels(factor(1:7,labels = c("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"))))
+
+table(doweek)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
